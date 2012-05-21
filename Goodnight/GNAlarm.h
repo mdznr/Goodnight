3f88be45ebd36ca1cf0f956547fe36bd2839b2e5
Matt Zanchelli <matt@mdznr.com>
2012-05-20 23:30:17 -0400

    Initial commit

---
+++ Goodnight/GNAlarm.h
@@ -0,0 +1,13 @@
+//
+//  GNAlarm.h
+//  Goodnight
+//
+//  Created by Matt on 5/20/12.
+//  Copyright (c) 2012 Matt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GNAlarm : UILocalNotification
+
+@end
---
+++ Goodnight/GNAlarm.m
@@ -0,0 +1,13 @@
+//
+//  GNAlarm.m
+//  Goodnight
+//
+//  Created by Matt on 5/20/12.
+//  Copyright (c) 2012 Matt. All rights reserved.
+//
+
+#import "GNAlarm.h"
+
+@implementation GNAlarm
+
+@end
