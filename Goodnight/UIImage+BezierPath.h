fb9a4b81bcdb0464fd2599de2a4dad41c0ad7b22
Matt Zanchelli <matt@mdznr.com>
2013-09-15 02:40:46 -0400

    Adding documentation to UIImage+BezierPath

--- Goodnight/UIImage+BezierPath.h
+++ Goodnight/UIImage+BezierPath.h
@@ -9,10 +9,14 @@
 
 @interface UIImage (BezierPath)
 
-///
+/// Create an image with a @c UIBezierPath and fill with a specified color.
+/// @parameter bezierPath The @c UIBezierPath to draw into image
+/// @parameter color The color to fill the @c bezierPath
 + (UIImage *)imageWithBezierPathFill:(UIBezierPath *)bezierPath withColor:(UIColor *)color;
 
-///
+/// Create an image with a @c UIBezierPath and stroke with a specified color
+/// @parameter bezierPath The @c UIBezierPath to draw into image
+/// @parameter color The color to fill the @c bezierPath
 + (UIImage *)imageWithBezierPathStroke:(UIBezierPath *)bezierPath withColor:(UIColor *)color;
 
 @end
